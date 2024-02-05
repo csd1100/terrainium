fb908a433a5359211857e803c1a8c3977c02c110
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2024-02-05 12:04:11 +0530

    feat(#6): fix process status store

--- src/bin/terrainium_executor.rs
+++ src/bin/terrainium_executor.rs
@@ -1,41 +1,41 @@
 use std::{fs::File, path::PathBuf, process::Command};
 
-use anyhow::{Ok, Result};
+use anyhow::{Context, Ok, Result};
 use clap::Parser;
 use terrainium::{
     handlers::helpers::get_process_log_file_path,
     types::executor::{Executable, ExecutorArgs, Status},
 };
 
-fn create_log_files(
-    session_id: &String,
-    process_uuid: &String,
-) -> Result<(File, File, PathBuf, PathBuf)> {
-    let out_file = format!("std_out-{}.log", process_uuid);
-    let err_file = format!("std_err-{}.log", process_uuid);
-    let out_file = &get_process_log_file_path(session_id, out_file)?;
-    let err_file = &get_process_log_file_path(session_id, err_file)?;
-
-    std::fs::write(&out_file, "")?;
-    std::fs::write(&err_file, "")?;
-
+fn create_log_file(session_id: &String, filename: String) -> Result<(File, PathBuf)> {
+    let out_file = &get_process_log_file_path(session_id, filename.clone())
+        .context(format!("Unable to get log file path: {}", filename))?;
     let out = File::options()
         .append(true)
         .create_new(true)
-        .open(&out_file)?;
-    let err = File::options()
-        .append(true)
-        .create_new(true)
-        .open(&err_file)?;
+        .open(&out_file)
+        .context(format!("Unable to create and open file:{:?}", &out_file))?;
+    return Ok((out, out_file.to_path_buf()));
+}
 
-    return Ok((out, err, out_file.to_path_buf(), err_file.to_path_buf()));
+fn create_log_files(
+    session_id: &String,
+    process_uuid: &String,
+) -> Result<((File, PathBuf), (File, PathBuf))> {
+    let out_file = format!("std_out-{}.log", process_uuid);
+    let err_file = format!("std_err-{}.log", process_uuid);
+    return Ok((
+        create_log_file(session_id, out_file)?,
+        create_log_file(session_id, err_file)?,
+    ));
 }
 
 fn main() -> Result<()> {
     let cli = ExecutorArgs::parse();
     let string_arg = cli.exec;
     let command: Executable = serde_json::from_str(&string_arg)?;
-    let (out, err, outfile_path, errfile_path) = create_log_files(&cli.id, &command.uuid)?;
+    let ((out, outfile_path), (err, errfile_path)) = create_log_files(&cli.id, &command.uuid)
+        .context("Unable to get stdout and stderr files")?;
 
     let mut cmd = Command::new(&command.exe);
     cmd.envs(std::env::vars()).stdout(out).stderr(err);
@@ -49,29 +49,47 @@ fn main() -> Result<()> {
         &command.exe, &command.args
     ));
 
-    let status_file = format!("status{}.json", command.uuid);
-    let status_file = get_process_log_file_path(&cli.id, status_file)?;
+    let status_file_name = format!("status-{}.json", command.uuid);
+    let status_file_path = get_process_log_file_path(&cli.id, status_file_name.clone())?;
+    {
+        let (status_file, _) = create_log_file(&cli.id, status_file_name)?;
 
-    let child_stat = serde_json::to_string(&Status {
-        uuid: command.uuid,
-        pid: child.id(),
-        cmd: command.exe,
-        args: command.args,
-        stdout_file: outfile_path,
-        stderr_file: errfile_path,
-        ec: None,
-    })?;
-
-    std::fs::write(&status_file, child_stat)?;
+        serde_json::to_writer_pretty(
+            &status_file,
+            &Status {
+                uuid: command.uuid,
+                pid: child.id(),
+                cmd: command.exe,
+                args: command.args,
+                stdout_file: outfile_path,
+                stderr_file: errfile_path,
+                ec: None,
+            },
+        )?;
+    }
 
     let output = child.wait_with_output()?;
 
     // rewrite json with exit code status
-    let stats = std::fs::read_to_string(&status_file)?;
-    let mut stats: Status = serde_json::from_str(&stats)?;
-    stats.ec = Some(output.status.to_string());
-    let stats = serde_json::to_string(&stats)?;
-    std::fs::write(&status_file, &stats)?;
+    let mut existing_status: Status;
+    {
+        let status_file = File::open(&status_file_path).context(format!(
+            "Unable to open file:{:?}, for reading",
+            &status_file_path
+        ))?;
+        existing_status = serde_json::from_reader(status_file)?;
+    }
+
+    existing_status.ec = Some(output.status.to_string());
+
+    let status_file = File::options()
+        .write(true)
+        .open(&status_file_path)
+        .context(format!(
+            "Unable to open file:{:?} to update exit code",
+            &status_file_path
+        ))?;
+    serde_json::to_writer_pretty(&status_file, &existing_status)?;
 
     return Ok(());
 }
