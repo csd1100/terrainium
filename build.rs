177becb0d224557e23fbb9101ca1fceb8e0abb26
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2024-02-22 17:03:46 +0530

    feat(#15): rename proto execute to activate

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() -> Result<()> {
     prost_build::compile_protos(
         &[
             "proto/terrainium/v1/command.proto",
-            "proto/terrainium/v1/execute.proto",
+            "proto/terrainium/v1/activate.proto",
             "proto/terrainium/v1/status.proto",
         ],
         &["proto/"],
