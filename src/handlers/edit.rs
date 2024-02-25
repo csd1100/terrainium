1d9f68faf577157e48afa3d970ac01b7ca9eaeaa
Chaitanya Deshpande <chaitanya.deshpande.1100@gmail.com>
2024-02-25 15:03:07 +0530

    refactor: fix edit test

--- src/handlers/edit.rs
+++ src/handlers/edit.rs
@@ -15,86 +15,102 @@ pub fn handle() -> Result<()> {
     Ok(())
 }
 
-// #[cfg(test)]
-// mod test {
-//
-//     use anyhow::Result;
-//     use mockall::predicate::eq;
-//     use serial_test::serial;
-//     use std::path::PathBuf;
-//
-//     use crate::{
-//         helpers::operations::mock_fs,
-//         shell::{editor::mock_edit, zsh::mock_ops},
-//         types::{args::BiomeArg, terrain::test_data},
-//     };
-//
-//     #[test]
-//     #[serial]
-//     fn handle_edit_opens_editor_and_compiles_scripts() -> Result<()> {
-//         let mock_get_toml_path = mock_fs::get_current_dir_toml_context();
-//         mock_get_toml_path
-//             .expect()
-//             .return_once(|| Ok(PathBuf::from("./example_configs/terrain.full.toml")))
-//             .times(1);
-//
-//         let mock_edit_file = mock_edit::file_context();
-//         mock_edit_file
-//             .expect()
-//             .with(eq(PathBuf::from("./example_configs/terrain.full.toml")))
-//             .return_once(|_| Ok(()))
-//             .times(1);
-//
-//         let get_central_store_path_context = mock_fs::get_central_store_path_context();
-//         get_central_store_path_context
-//             .expect()
-//             .return_once(|| Ok(PathBuf::from("~/.config/terrainium/terrains/")))
-//             .times(1);
-//
-//         let remove_all_script_files = mock_fs::remove_all_script_files_context();
-//         remove_all_script_files
-//             .expect()
-//             .withf(|path| path == PathBuf::from("~/.config/terrainium/terrains/").as_path())
-//             .return_once(|_| Ok(()))
-//             .times(1);
-//
-//         let terrain = test_data::terrain_full();
-//         let main = terrain.get(Some(BiomeArg::None))?;
-//         let generate_and_compile_context = mock_ops::generate_and_compile_context();
-//         generate_and_compile_context
-//             .expect()
-//             .with(
-//                 eq(PathBuf::from("~/.config/terrainium/terrains/")),
-//                 eq(String::from("none")),
-//                 eq(main),
-//             )
-//             .return_once(|_, _, _| Ok(()))
-//             .times(1);
-//
-//         let example_biome = terrain.get(Some(BiomeArg::Value("example_biome".to_owned())))?;
-//         generate_and_compile_context
-//             .expect()
-//             .with(
-//                 eq(PathBuf::from("~/.config/terrainium/terrains/")),
-//                 eq(String::from("example_biome")),
-//                 eq(example_biome),
-//             )
-//             .return_once(|_, _, _| Ok(()))
-//             .times(1);
-//
-//         let example_biome2 = terrain.get(Some(BiomeArg::Value("example_biome2".to_owned())))?;
-//         generate_and_compile_context
-//             .expect()
-//             .with(
-//                 eq(PathBuf::from("~/.config/terrainium/terrains/")),
-//                 eq(String::from("example_biome2")),
-//                 eq(example_biome2),
-//             )
-//             .return_once(|_, _, _| Ok(()))
-//             .times(1);
-//
-//         super::handle()?;
-//
-//         Ok(())
-//     }
-// }
+#[cfg(test)]
+mod test {
+
+    use anyhow::Result;
+    use mockall::predicate::eq;
+    use serial_test::serial;
+    use tempfile::tempdir;
+    use std::path::{Path, PathBuf};
+
+    use crate::{
+        helpers::utils::mock_fs, shell::{editor::mock_edit, zsh::mock_ops}, types::{args::BiomeArg, terrain::test_data}
+    };
+
+    #[test]
+    #[serial]
+    fn handle_edit_opens_editor_and_compiles_scripts() -> Result<()> {
+        let test_dir = tempdir()?;
+        let test_dir_path: PathBuf = test_dir.path().into();
+        let mock_cwd = mock_fs::get_cwd_context();
+        mock_cwd
+            .expect()
+            .returning(move || {
+                let test_dir_path: PathBuf = test_dir_path.clone();
+                Ok(test_dir_path)
+            })
+            .times(3);
+
+        let home_dir = tempdir()?;
+        let home_dir_path: PathBuf = home_dir.path().into();
+        let mock_home = mock_fs::get_home_dir_context();
+        mock_home
+            .expect()
+            .returning(move || {
+                let home_dir_path: PathBuf = home_dir_path.clone();
+                Ok(home_dir_path)
+            })
+            .times(1);
+
+        let mut terrain_toml_path: PathBuf = test_dir.path().into();
+        terrain_toml_path.push("terrain.toml");
+
+        std::fs::copy("./example_configs/terrain.full.toml", &terrain_toml_path)?;
+
+        let mock_edit_file = mock_edit::file_context();
+        mock_edit_file
+            .expect()
+            .with(eq(terrain_toml_path))
+            .return_once(|_| Ok(()))
+            .times(1);
+
+        let home_dir_path: PathBuf = home_dir.path().into();
+        let test_dir_path: PathBuf = test_dir.path().into();
+        let scripts_dir_name = Path::canonicalize(test_dir_path.as_path())?
+            .to_string_lossy()
+            .to_string()
+            .replace('/', "_");
+        let scripts_dir_path = home_dir_path.join(PathBuf::from(
+            ".config/terrainium/terrains/".to_owned() + &scripts_dir_name,
+        ));
+        let terrain = test_data::terrain_full();
+        let main = terrain.get(Some(BiomeArg::None))?;
+        let generate_and_compile_context = mock_ops::generate_and_compile_context();
+        generate_and_compile_context
+            .expect()
+            .with(
+                eq(scripts_dir_path.clone()),
+                eq(String::from("none")),
+                eq(main),
+            )
+            .return_once(|_, _, _| Ok(()))
+            .times(1);
+
+        let example_biome = terrain.get(Some(BiomeArg::Value("example_biome".to_owned())))?;
+        generate_and_compile_context
+            .expect()
+            .with(
+                eq(scripts_dir_path.clone()),
+                eq(String::from("example_biome")),
+                eq(example_biome),
+            )
+            .return_once(|_, _, _| Ok(()))
+            .times(1);
+
+        let example_biome2 = terrain.get(Some(BiomeArg::Value("example_biome2".to_owned())))?;
+        generate_and_compile_context
+            .expect()
+            .with(
+                eq(scripts_dir_path.clone()),
+                eq(String::from("example_biome2")),
+                eq(example_biome2),
+            )
+            .return_once(|_, _, _| Ok(()))
+            .times(1);
+
+        super::handle()?;
+
+        Ok(())
+    }
+}
