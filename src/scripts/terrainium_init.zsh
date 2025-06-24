628ab1662a6c8bf0a53ff003090c270bf9505317
Chaitanya Deshpande <cdeshpande@proton.me>
2025-06-24 22:38:01 +0530

    fix(#81): do not export terrainium commands to stop nested shell created outside terrain directory to throw errors

--- src/scripts/terrainium_init.zsh
+++ src/scripts/terrainium_init.zsh
@@ -22,6 +22,14 @@ if [ "$TERRAIN_ENABLED" = "true" ]; then
     "${terrain_init}"
     builtin unfunction -- "${terrain_init}"
     __terrainium_enter
+    # unexport but set terrainium env vars
+    typeset +x TERRAIN_ENABLED
+    typeset +x TERRAIN_SESSION_ID
+    typeset +x TERRAIN_SELECTED_BIOME
+    typeset +x TERRAIN_AUTO_APPLY
+    typeset +x TERRAIN_DIR
+    typeset +x TERRAIN_INIT_SCRIPT
+    typeset +x terrain_init
 else
     chpwd_functions=(__terrainium_chpwd_functions $chpwd_functions)
     __terrainium_auto_apply
